1cba5e2bbb6e7f107dc78e3a3d315a9258daed84
I.Karpenko <i.i.kontent@gmail.com>
2025-08-04 23:04:10 +0300

    HomeWork-6-04.08.2025

--- src/test/java/org/prog/session6/TestNG_HW6.java
+++ src/test/java/org/prog/session6/TestNG_HW6.java
@@ -1,5 +1,34 @@
 package org.prog.session6;
 
-public class TestNG_HW6 {
+import java.util.*;
 
+public class TestNG_HW6 {
+    public static void main(String[] args) {
+        // tax id, each tax id can have 1-5 FOPs
+        Map<String, List<String>> map = new HashMap<>();
+        map.put("17253761525", new ArrayList<>());
+
+        map.get("17253761525").add("FOP #1");
+        map.get("17253761525").add("FOP #2");
+        map.get("17253761525").add("FOP #3");
+        map.get("17253761525").add("FOP #4");
+
+        List<String> list1 = map.get("17253761525");
+
+        for (String someString : list1) {
+            System.out.println("Citizen 17253761525 has : " + someString);
+        }
+    }
+
+    public static String randomColor() {
+        Random random = new Random();
+        return switch (random.nextInt(5)) {
+            case 0 -> "red";
+            case 1 -> "green";
+            case 2 -> "blue";
+            case 3 -> "yellow";
+            case 4 -> "pink";
+            default -> "black";
+        };
+    }
 }
